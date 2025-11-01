7603a44c30a15ea5d456ea84fa23f22001d39822
Artem Strelnikov <piramida1237@gmail.com>
2025-11-01 19:41:33 +0100

    array

--- src/main.ts
+++ src/main.ts
@@ -25,3 +25,23 @@ const product: Product = {
 };
 
 console.log(`Product: ${JSON.stringify(product)}`);
+
+//!task-3
+
+const usernames: string[] = ["alice", "bob", "charlie"];
+
+const ratings: number[] = [4.5, 3.8, 5];
+
+interface Products {
+  id: number;
+  title: string;
+}
+
+const products: Products[] = [
+  { id: 1, title: "Phone" },
+  { id: 2, title: "Laptop" },
+];
+
+console.log(`Usernames: ${JSON.stringify(usernames)}`);
+console.log(`Ratings: ${JSON.stringify(ratings)}`);
+console.log(`Products: ${JSON.stringify(products)}`);
