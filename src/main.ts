7156326ff18521a448277fd5c9a28a4e11c7dc0e
Artem Strelnikov <piramida1237@gmail.com>
2025-11-08 17:40:17 +0100

    mmmo

--- src/main.ts
+++ src/main.ts
@@ -150,3 +150,26 @@ const stringList: List<string> = {
     return this.items[index];
   },
 };
+
+//?
+
+interface ApiResponse<T> {
+  data: T;
+  status: number;
+}
+
+const userResponse: ApiResponse<{ id: number; name: string }> = {
+  data: {
+    id: 1,
+    name: "Alice",
+  },
+  status: 200,
+};
+
+const invoiceResponse: ApiResponse<{ email: string; amount: number }> = {
+  data: {
+    email: "alice@mail.com",
+    amount: 150,
+  },
+  status: 201,
+};
